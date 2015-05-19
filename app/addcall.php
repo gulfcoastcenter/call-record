dae218342a8cf155ac710ad3d7d49ce323af07f6
Tim Markello <timmarkello@gmail.com>
2015-05-19 15:52:51 -0500

    new file:   app/addcall.php + function to insert new call into data base

---
+++ app/addcall.php
@@ -0,0 +1,35 @@
+<?php
+
+function addcall($config, $program, $postdata) {
+   require_once('db/connect.php');
+   require_once('db/execute.php');
+   require_once('obj/Call.php');
+   require_once('obj/CallList.php');
+
+   $conn = connect($config);
+
+   $sql = 'sp_addcall';
+   
+   //$args->status = 0;
+   $args->historyid = null;
+   $args->programid = $program ? $program : null;
+   $args->calldate = $postdata['date'];
+   $args->callername = $postdata['caller'];
+   $args->patientname = $postdata['patient'];
+   $args->cmhcid = $postdata['cmhcid'];
+   $args->phonenumber = $postdata['phone'];
+   $args->County = $postdata['county'];
+   $args->street = $postdata['street'];
+   $args->state = $postdata['state'];
+   $args->zip = $postdata['zip'];
+   $args->referredto = $postdata['referredto'];
+   $args->referredfrom = $postdata['referredfrom'];
+   $args->request = $postdata['request'];
+   
+   return json_encode(Call::newCall(execute($conn, $sql, $args)[0]));
+}
+
+?>
+
+
+
