8fea7006aeb45ad31eb913f327a0a266a75b2a2e
Tim Markello <timmarkello@gmail.com>
2015-05-19 15:49:32 -0500

    modified:   obj/Call.php ~ finish implementing all needed fields for the Call object ~ implement jsonserialization for Call object

--- obj/Call.php
+++ obj/Call.php
@@ -1,24 +1,31 @@
 <?php
 
-class Call {
+class Call implements JsonSerializable {
    public function __construct($data) {
       $this->id = $data[0];
       $this->date = $data[1];
       $this->caller = $data[2];
-      $this->program = $data[3];
-      $this->patient = $data[4];
-      $this->cmhcid = $data[5];
-      $this->phone = $data[6];
-      $this->county = $data[7];
-      $this->street = $data[8];
-      $this->state = $data[9];
-      $this->zip = $data[10];
-      $this->referredto = $data[11];
-      $this->referredfrom = $data[12];
-      $this->request = $data[13];
+      $this->status = $data[3];
+      $this->historyid = $data[4];
+      $this->program = $data[5];
+      $this->patient = $data[6];
+      $this->cmhcid = $data[7];
+      $this->phone = $data[8];
+      $this->county = $data[9];
+      $this->street = $data[10];
+      $this->state = $data[11];
+      $this->zip = $data[12];
+      $this->referredto = $data[13];
+      $this->referredfrom = $data[14];
+      $this->request = $data[15];
    }
+
    public static function newCall($data) { 
       return new Call($data);
+   }
+
+   public function jsonSerialize() { 
+      return $this;
    }
 }
 
